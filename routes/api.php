674bb9c439f8c8d2b02ae755c70d489e42bcba70
Marcos <marcosds123456@gmail.com>
2026-01-03 22:51:37 -0300

    chore: init api config

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::prefix('v1')->group(function () {
+    Route::get('/', function () {
+        return response()->json([
+            'message' => 'Welcome to TechID Backend API!',
+            'status' => 'success',
+        ]);
+    });
+});
+
+
+
+?>
